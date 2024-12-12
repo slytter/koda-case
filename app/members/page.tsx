0fcb45d3e4be83656ffc2557981379098fc3484f
Schlüter <nikolaj.sn@hotmail.com>
2024-12-12 17:21:41 +0100

    tryfix disable caching

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -1,4 +1,3 @@
-"use server"
 import { getMembers } from "../controllers/member.controller"
 const buttonClass = "rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 my-4"
 
