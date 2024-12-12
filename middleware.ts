6df13d7bcc432fd24af7307aa5de1b6d572d853d
Schlüter <nikolaj.sn@hotmail.com>
2024-12-12 16:56:30 +0100

    tryfix disable caching

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+    // Log the requested path to see if it's matching correctly
+    console.log(`Request Path: ${req.nextUrl.pathname}`);
+
+    // Apply middleware to the /members path
+    if (req.nextUrl.pathname.startsWith('/members')) {
+    }
+    const response = NextResponse.next();
+    console.log('Cache-Control header set for /members');
+    response.headers.set('Cache-Control', 'no-store');
+    response.headers.set('cache', 'no-store');
+
+    return response;
+      
+    // For other paths, just proceed
+    return NextResponse.next();
+}
