00949dfadaec72fbc121f8c0e82964955d0938d8
Schlüter <nikolaj.sn@hotmail.com>
2024-12-12 16:50:05 +0100

    tryfix disable caching

--- app/members/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function middleware(req: NextRequest) {
-    const response = NextResponse.next();
-    console.log('HIT MIDDLEWARE')
-    response.headers.set('Cache-Control', 'no-cache');
-    return response;
-}
